0b517daaf07e781a55e4d0b07b13f446772790c7
Lasoloz <lasolozdev@gmail.com>
2024-01-07 00:38:43 +0200

    [level1] Part 2 + refactoring

--- level1/level1.go
+++ level1/level1.go
@@ -2,30 +2,13 @@ package main
 
 import "fmt"
 
+const SUM = 2020
+
 func main() {
 	expenses := readExpenses()
-	answer, err := calculateAnswer(expenses)
 
-	if err != nil {
-		fmt.Println("No answer", err)
-	} else {
-		fmt.Println(answer)
-	}
-}
-
-func calculateAnswer(expenses []uint) (answer uint, err error) {
-	for index, expense1 := range expenses {
-		for _, expense2 := range expenses[index+1:] {
-			if expense1+expense2 == 2020 {
-				answer = expense1 * expense2
-
-				return
-			}
-		}
-	}
-
-	err = fmt.Errorf("no answer")
-	return
+	printAnswer(calculate1(expenses))
+	printAnswer(calculate2(expenses))
 }
 
 func readExpenses() []uint {
@@ -42,4 +25,41 @@ func readExpenses() []uint {
 		list = append(list, expense)
 	}
 	return list
+}
+
+func calculate1(expenses []uint) (answer uint, err error) {
+	for index, expense1 := range expenses {
+		for _, expense2 := range expenses[index+1:] {
+			if expense1+expense2 == SUM {
+				answer = expense1 * expense2
+
+				return
+			}
+		}
+	}
+
+	err = fmt.Errorf("no answer for part 1")
+	return
+}
+
+func calculate2(expenses []uint) (uint, error) {
+	for index1, expense1 := range expenses {
+		for index2, expense2 := range expenses[index1+1:] {
+			for _, expense3 := range expenses[index2+1:] {
+				if expense1+expense2+expense3 == SUM {
+					return expense1 * expense2 * expense3, nil
+				}
+			}
+		}
+	}
+
+	return 0, fmt.Errorf("no answer for part 2")
+}
+
+func printAnswer(answer uint, err error) {
+	if err != nil {
+		fmt.Println("No answer", err)
+	} else {
+		fmt.Println(answer)
+	}
 }
