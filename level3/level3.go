ea51f1c8719c9befb40e9b644902770705f2df23
Lasoloz <lasolozdev@gmail.com>
2024-01-07 21:06:14 +0200

    [level3] Part 2

--- level3/level3.go
+++ level3/level3.go
@@ -6,6 +6,13 @@ const stepX = 3
 const stepY = 1
 const tree = '#'
 
+type step struct {
+	x int
+	y int
+}
+
+var steps = []step{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}}
+
 type slopeSection struct {
 	lines  []string
 	width  int
@@ -15,12 +22,13 @@ type slopeSection struct {
 func main() {
 	slope := readSlope()
 
-	fmt.Println(countTrees(slope))
+	fmt.Println(countTrees(slope, stepX, stepY))
+	fmt.Println(countTreeConfigurations(slope))
 }
 
-func countTrees(slope slopeSection) int {
+func countTrees(slope slopeSection, dx, dy int) int {
 	width, height := slope.width, slope.height
-	x, y := stepX%width, stepY
+	x, y := dx%width, dy
 
 	trees := 0
 
@@ -29,11 +37,21 @@ func countTrees(slope slopeSection) int {
 			trees++
 		}
 
-		x = (x + stepX) % width
-		y = y + stepY
+		x = (x + dx) % width
+		y = y + dy
 	}
 
 	return trees
+}
+
+func countTreeConfigurations(slope slopeSection) int {
+	multipliedHits := 1
+
+	for _, step := range steps {
+		multipliedHits *= countTrees(slope, step.x, step.y)
+	}
+
+	return multipliedHits
 }
 
 func readSlope() slopeSection {
